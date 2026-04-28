cd8cc1b0ee2b1d3b71777859f8007e68d79dafba
WilliamKallin <kallinwilliam@gmail.com>
2026-04-28 09:50:52 +0200

    Added new endpoint

--- api/Controllers/AdminController.cs
+++ api/Controllers/AdminController.cs
@@ -18,4 +18,12 @@ public class AdminController(IUnitOfWork uow, IMapper mapper) : ApiBaseControlle
 
         return BadRequest();
     }
+
+    [HttpGet("test")]
+    public async Task<ActionResult> TestDeployment()
+    {
+        return Ok(new{Message = "Det funkar fruktansvärt bra!;D"});
+    }
 }
+
+
